8339c42e8c91e30f8889f5d8848dc24c6dd294ea
Otavio Vasconcelos <otaviocavasc2@gmail.com>
2023-02-27 15:32:01 -0300

    Implemented lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+# url = 'http://tmdb.lewagon.com/movie/top_rated'
+# img_url = 'https://image.tmdb.org/t/p/original'
+
+# response = JSON.parse(URI.open(url).read)
+# response['results'].each do |movie|
+#   Movie.create(
+#     title: movie['original_title'],
+#     overview: movie['overview'],
+#     poster_url: "#{img_url}#{movie['poster_path']}",
+#     rating: movie['vote_average']
+#   )
+# end
+List.create(name: "Classic movies")
+List.create(name: "Thriller movies")
