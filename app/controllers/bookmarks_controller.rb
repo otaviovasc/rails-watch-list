16f0b98aef00b234024e5a124f892e29a5c0af0b
Otavio Vasconcelos <otaviocavasc2@gmail.com>
2023-02-28 11:14:23 -0300

    implemented new bookmark view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,17 @@
 class BookmarksController < ApplicationController
+  def new
+    @movies = Movie.all
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark.new(bookmark_params)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
 end
